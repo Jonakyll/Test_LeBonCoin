3213249a230e68033aff83a89d1297e84fd5445f
CHU Jonathan <46070603+Jonakyll@users.noreply.github.com>
2021-01-12 16:49:00 +0100

    récupération et affichage de l'objet json

--- app/src/main/java/fr/umlv/testleboncoin/MainActivity.kt
+++ app/src/main/java/fr/umlv/testleboncoin/MainActivity.kt
@@ -2,10 +2,31 @@ package fr.umlv.testleboncoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        show_albums_button.setOnClickListener {
+            val queue = Volley.newRequestQueue(this)
+
+            val stringRequest = StringRequest(
+                Request.Method.GET, Album.URL, { response ->
+                    response_text.text = SearchTask().execute(response).get().toString()
+                }, {
+                    Log.e(javaClass.name, "Error with Volley request", it)
+                }
+            )
+            queue.add(stringRequest)
+        }
     }
+
 }
